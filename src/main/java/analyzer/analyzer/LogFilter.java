386d715562c290a9c934bbcdc1fd701a3ad53b35
Kiryll Kurda <kurdakirill6@gmail.com>
2024-11-08 00:09:20 +0300

    feat: implement LogFilter for filtering Nginx log entries based on date and field values

---
+++ src/main/java/analyzer/analyzer/LogFilter.java
@@ -0,0 +1,50 @@
+package analyzer.analyzer;
+
+import analyzer.config.AnalyzerConfig;
+import analyzer.model.NginxLogEntry;
+import java.time.LocalDateTime;
+import java.util.regex.Pattern;
+
+public class LogFilter {
+    private final AnalyzerConfig analyzerConfig;
+
+    public LogFilter(AnalyzerConfig analyzerConfig) {
+        this.analyzerConfig = analyzerConfig;
+    }
+
+    public boolean isWithinRange(LocalDateTime dateTime) {
+        boolean withinFromRange = analyzerConfig.from()
+            .map(date -> !dateTime.isBefore(date))
+            .orElse(true);
+
+        boolean withinToRange = analyzerConfig.to()
+            .map(date -> !dateTime.isAfter(date))
+            .orElse(true);
+
+        return withinFromRange && withinToRange;
+    }
+
+    public boolean matchByFieldAndValue(NginxLogEntry entry) {
+        if (analyzerConfig.filterField().isEmpty() || analyzerConfig.filterValue().isEmpty()) {
+            return true;
+        }
+
+        String actualFilterValue = switch (analyzerConfig.filterField().get().toLowerCase()) {
+            case "agent" -> entry.httpUserAgent();
+            case "status" -> String.valueOf(entry.statusCode());
+            case "resource" -> entry.extractResource().orElse(null);
+            case "method" -> entry.extractHttpMethod();
+            case "ip" -> entry.clientIP();
+            default -> null;
+        };
+
+        if (actualFilterValue == null) {
+            return false;
+        }
+
+        String filterValue = analyzerConfig.filterValue().get();
+        String regex = filterValue.replace("*", ".*");
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        return pattern.matcher(actualFilterValue).matches();
+    }
+}
